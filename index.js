1db947d96c9cd346063b8e3ecbf4d573311ea416
Patrick A <akolo@hotmail.com>
2020-11-17 21:53:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 document.getElementById('main').remove();
+=======
+main.remove();
+>>>>>>> 050276690aa4508621ce88a1148fa7cd9fa95b2c
 
 let newHeader = document.createElement("h1");
 
 newHeader.setAttribute("id","victory");
 
+<<<<<<< HEAD
 newHeader.innerHTML =  "Patrick is the champion";
+=======
+newHeader.innerHTML =  "Patrick is the champion";
+>>>>>>> 050276690aa4508621ce88a1148fa7cd9fa95b2c
